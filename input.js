37cfa49d17f3b2b3ca6e1ed1d5b6ee8829add693
Bita <bita.janzadeh@hotmail.com>
2021-12-01 19:35:46 -0500

    added messages and wasd movement

--- input.js
+++ input.js
@@ -1,8 +1,10 @@
 const net = require("net");
 const {connect} = require("./client.js");
+let connection;
 
 const conn = connect();
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -19,9 +21,41 @@ const handleUserInput = function (keyPressed) {
   if (keyPressed === '\u0003') {
     process.exit();
   }
-  setInterval(() => {
+
+  if (keyPressed === "w") {
     conn.write("Move: up");
-  }, 1000);
+  }
+  if (keyPressed === "a") {
+    conn.write("Move: left");
+  }
+  if (keyPressed === "s") {
+    conn.write("Move: down");
+  }
+  if (keyPressed === "d") {
+    conn.write("Move: right");
+  }
+
+  if (keyPressed === "z") {
+    conn.write("Say: im a w0t?");
+  }
+  if (keyPressed === "x") {
+    conn.write("Say: omg");
+  }
+  if (keyPressed === "c") {
+    conn.write("Say: omg");
+  }
+  if (keyPressed === "v") {
+    conn.write("Say: ahhhhh");
+  }
+  if (keyPressed === "b") {
+    conn.write("Say: proud of myself");
+  }
+
+
+
+/*   setInterval(() => {
+    conn.write("Move: up");
+  }, 1000); */
 };
 
 module.exports = {setupInput};
